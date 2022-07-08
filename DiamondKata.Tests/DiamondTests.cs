fb3dbd118cb5db8a10b7eea30df1267b2ac72106
liquidvapour <liquidvapour@gmail.com>
2022-07-08 18:21:03 +0100

    Nieve implementation works

--- DiamondKata.Tests/DiamondTests.cs
+++ DiamondKata.Tests/DiamondTests.cs
@@ -3,7 +3,7 @@ namespace DiamondKata.Tests;
 public class DiamondTests
 {
     [Fact]
-    public void when_given_A_return_A()
+    public void when_told_to_run_with_A_will_return_A()
     {
         var result = Diamond.Run("A");
         Assert.Equal("A", result);
--- DiamondKata/Diamond.cs
+++ DiamondKata/Diamond.cs
@@ -4,6 +4,11 @@ public static class Diamond
 {
     public static string Run(string s)
     {
+        return s;
+    }
+
+    public static string GetCharacters(string s)
+    {
         throw new NotImplementedException();
     }
 }
