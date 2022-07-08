e831fd722a39c716e6cd2250d5b5d97cd3086da3
liquidvapour <liquidvapour@gmail.com>
2022-07-08 20:40:26 +0100

    add GetCharacters and GetCharacterForColumn

--- DiamondKata.Tests/DiamondTests.cs
+++ DiamondKata.Tests/DiamondTests.cs
@@ -8,4 +8,40 @@ public class DiamondTests
         var result = Diamond.Run("A");
         Assert.Equal("A", result);
     }
+
+    [Fact]
+    public void when_told_to_get_characters_with_B_will_return_AB()
+    {
+        var result = Diamond.GetCharacters('B');
+        Assert.Equal("AB", result);
+    }
+
+    [Fact]
+    public void when_told_to_get_characters_with_C_will_return_ABC()
+    {
+        var result = Diamond.GetCharacters('C');
+        Assert.Equal("ABC", result);
+    }
+
+    [Fact]
+    public void when_told_to_render_line_A_with_max_length_3_will_return_xxAxx()
+    {
+        var result = Diamond.RenderLine('A', 3);
+        Assert.Equal("  A  ", result);
+    }
+
+    [Theory]
+    [InlineData('A', 0, 'A')]
+    [InlineData('B', 0, ' ')]
+    [InlineData('B', 1, 'B')]
+    [InlineData('B', 2, ' ')]
+    [InlineData('C', 0, ' ')]
+    [InlineData('C', 1, ' ')]
+    [InlineData('C', 2, 'C')]
+    [InlineData('C', 3, ' ')]
+    public void when_told_to_get_char_for_c_with_len_for_index_will_return(char c, int index, char expected)
+    {
+        var result = Diamond.GetCharacterForColumn(c, index);
+        Assert.Equal(expected, result);
+    }
 }
--- DiamondKata/Diamond.cs
+++ DiamondKata/Diamond.cs
@@ -2,13 +2,30 @@
 
 public static class Diamond
 {
+    private const int FirstCharacter = 'A';
+
     public static string Run(string s)
     {
         return s;
     }
 
-    public static string GetCharacters(string s)
+    public static string GetCharacters(char s)
+    {
+        var numberOfCharacters = (s - FirstCharacter) + 1;
+
+        var result = new char[numberOfCharacters];
+        for (var i = 0; i < numberOfCharacters; i++)
+        {
+            result[i] = (char)(FirstCharacter + i);
+        }
+
+        return new string(result);
+    }
+
+    public static string RenderLine(char character, int maxCharacterCount)
     {
         throw new NotImplementedException();
     }
+
+    public static char GetCharacterForColumn(char c, int i) => c - FirstCharacter == i ? c : ' ';
 }
