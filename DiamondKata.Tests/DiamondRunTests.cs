2ce341525bce4b5af85e2738be4ce377fe3c9cfd
liquidvapour <liquidvapour@gmail.com>
2022-07-10 20:42:19 +0100

    pull out Run tests to own file

---
+++ DiamondKata.Tests/DiamondRunTests.cs
@@ -0,0 +1,41 @@
+﻿namespace DiamondKata.Tests;
+
+public class DiamondRunTests
+{
+    [Fact]
+    public void when_told_to_run_with_A_will_return_A()
+    {
+        var result = Diamond.Run('A');
+        Assert.Equal("A" + Environment.NewLine, result);
+    }
+
+    [Fact]
+    public void when_told_to_run_with_B_will_return_correct_result()
+    {
+        var result = Diamond.Run('B');
+        Assert.Equal(
+            " A " + Environment.NewLine +
+            "B B" + Environment.NewLine +
+            " A " + Environment.NewLine, 
+            result);
+    }
+
+    [Fact]
+    public void when_told_to_run_with_C_will_return_correct_result()
+    {
+        var result = Diamond.Run('C');
+        Assert.Equal(
+            "  A  " + Environment.NewLine +
+            " B B " + Environment.NewLine +
+            "C   C" + Environment.NewLine +
+            " B B " + Environment.NewLine +
+            "  A  " + Environment.NewLine, 
+            result);
+    }
+
+    [Fact]
+    public void when_told_to_run_with_a_will_throw_ArbumentException()
+    {
+        
+    }
+}
--- DiamondKata.Tests/DiamondTests.cs
+++ DiamondKata.Tests/DiamondTests.cs
@@ -3,37 +3,6 @@ namespace DiamondKata.Tests;
 public class DiamondTests
 {
     [Fact]
-    public void when_told_to_run_with_A_will_return_A()
-    {
-        var result = Diamond.Run('A');
-        Assert.Equal("A" + Environment.NewLine, result);
-    }
-
-    [Fact]
-    public void when_told_to_run_with_B_will_return_correct_result()
-    {
-        var result = Diamond.Run('B');
-        Assert.Equal(
-            " A " + Environment.NewLine +
-            "B B" + Environment.NewLine +
-            " A " + Environment.NewLine, 
-            result);
-    }
-
-    [Fact]
-    public void when_told_to_run_with_C_will_return_correct_result()
-    {
-        var result = Diamond.Run('C');
-        Assert.Equal(
-            "  A  " + Environment.NewLine +
-            " B B " + Environment.NewLine +
-            "C   C" + Environment.NewLine +
-            " B B " + Environment.NewLine +
-            "  A  " + Environment.NewLine, 
-            result);
-    }
-
-    [Fact]
     public void when_told_to_get_characters_with_B_will_return_AB()
     {
         var result = Diamond.GetCharacters('B');
